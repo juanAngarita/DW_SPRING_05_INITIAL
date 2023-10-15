1a8d5719a98764fb60f8321d7aa248f7a9fbaa9a
juanAngarita <angaritat.js@javeriana.edu.co>
2023-10-15 09:25:13 -0500

    first commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -7,9 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +36,7 @@ public class StudentController {
         return studentService.SearchAll();
     }
 
-    // http://localhost:8080/student/find?id=1
+    // http://localhost:8090/student/find?id=1
     @GetMapping("/find")
     public Student mostrarInfoEstudiante(@RequestParam("id") Long id) {
 
@@ -45,29 +45,34 @@ public class StudentController {
         return student;
     }
 
-    // http://localhost:8080/student/find/1
-    @GetMapping("/find/{id}")
+    // http://localhost:8090/student/find/1
     public Student mostrarInfoEstudiante2(@PathVariable("id") Long id) {
         Student student = studentService.SearchById(id);
         return student;
     }
 
+    // http://localhost:8090/student/add
     @PostMapping("/add")
-    public void agregarEstudiante(@RequestBody Student student){
-        studentService.add(student);
+    public Student agregarEstudiante(@RequestBody Student student){
+        Student newStudent = studentService.add(student);
+
+        return newStudent;
     }
 
     //delete
-    //http://localhost:8090/delete/1
+    //http://localhost:8090/student/delete/1
     @DeleteMapping("/delete/{id}")
     public void eliminarEstudiante(@PathVariable("id") Long id){
         studentService.deleteById(id);
     }
 
-
-    @PostMapping("/update/{id}")
-    public void actualizarEstudiante(@RequestBody Student student, @PathVariable("id") int id){
-        studentService.update(student);
+    //http://localhost:8090/student/update/1
+    @PutMapping("/update/{id}")
+    public Student actualizarEstudiante(@RequestBody Student student, @PathVariable("id") Long id){
+        Student studentFind = studentService.SearchById(id);
+        student.setId(studentFind.getId());
+        Student studentUpdated = studentService.update(student);
+        return studentUpdated;
     }
 
 
